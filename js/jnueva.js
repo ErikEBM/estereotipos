d595d65cabc77faecca7f65654d5358bdd540a24
erikEBM <howl_bm96@hotmail.com>
2016-10-04 22:24:00 -0500

    actualizacion4

--- js/jnueva.js
+++ js/jnueva.js
@@ -1,5 +1,7 @@
 var palette = document.getElementById("id_div"); 
 var nodo = document.getElementById("id_aside");
+var nodo2 = document.getElementById("id_section"); 
 palette.onclick = function (){
     nodo.classList.add("hide");
+    nodo2.classList.add("block");
 }
